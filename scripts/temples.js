4b7ddaaef7781d8c60d5b3391961c3f30e1780f3
Nathaniel-Stokes35 <Nathaniel.Stokes35@gmail.com>
2025-01-16 20:22:12 -0600

    added getdates.js script to the bottom of temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -18,3 +18,10 @@ function navigation_respond() {
         titles.style.display = "grid";  
     }
 }
+const year_container = document.getElementById('currentYear');
+const modified_container = document.getElementById('lastModified');
+const today = new Date();
+const modified_date = document.lastModified;
+
+year_container.innerHTML = today.getFullYear();
+modified_container.innerHTML = `Last Modified: ${modified_date}`;
